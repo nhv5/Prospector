e97ad0164b32d38192b7fbbb881f26774e15936a
Nicholas Vuono <nhv5@scarletmail.rutgers.edu>
2016-11-11 23:40:17 -0500

    Entire Round Playable!!!
    An entire round of the game is playable! Next up is making the game know
    when it’s over.

--- Assets/__Scripts/Card.cs
+++ Assets/__Scripts/Card.cs
@@ -13,18 +13,76 @@ public class Card : MonoBehaviour {
 	public List<GameObject> pipGOs = new List<GameObject>();
 	
 	public GameObject back;     // back of card;
-	public CardDefinition def;  // from DeckXML.xml		
+	public CardDefinition def;  // from DeckXML.xml
+
+    public SpriteRenderer[] spriteRenderers;
+    
+    void Start()
+    {
+        SetSortOrder(0);
+    }		
 
 	
 	// property
-	public bool faceUP {
+	public bool faceUp {
 		get {
 			return (!back.activeSelf);
 		}		
 		set {
 			back.SetActive(!value);
 		}
-	}	
+	}
+    
+    public void PopulateSpriteRenderers()
+    {
+        if(spriteRenderers == null || spriteRenderers.Length == 0)
+        {
+            spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+        }
+    }
+
+    public void SetSortingLayerName(string tSLN)
+    {
+        PopulateSpriteRenderers();
+
+        foreach(SpriteRenderer tSR in spriteRenderers)
+        {
+            tSR.sortingLayerName = tSLN;
+        }
+    }
+
+    public void SetSortOrder(int sOrd)
+    {
+        PopulateSpriteRenderers();
+
+        foreach (SpriteRenderer tSR in spriteRenderers)
+        {
+            if (tSR.gameObject == this.gameObject)
+            {
+                tSR.sortingOrder = sOrd;
+                continue;
+            }
+
+            switch (tSR.gameObject.name)
+            {
+                case "back":
+                    tSR.sortingOrder = sOrd + 2;
+                    break;
+
+                case "face":
+                default:
+                    tSR.sortingOrder = sOrd + 1;
+                    break;
+            }
+
+        }
+    }
+
+    virtual public void OnMouseUpAsButton()
+    {
+        print(name);
+    }
+    	
 } // class Card
 
 [System.Serializable]
--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -9,14 +9,218 @@ public class Prospector : MonoBehaviour {
 	public Deck					deck;
 	public TextAsset			deckXML;
 
+    public Layout layout;
+    public TextAsset layoutXML;
+    public Vector3 layoutCenter;
+    public float xOffset = 3;
+    public float yOffset = -2.5f;
+    public Transform layoutAnchor;
+
+    public CardProspector target;
+    public List<CardProspector> tableau;
+    public List<CardProspector> discardPile;
+
 	void Awake(){
 		S = this;
 	}
+
+    public List<CardProspector> drawPile;
 
 	void Start() {
 		deck = GetComponent<Deck> ();
 		deck.InitDeck (deckXML.text);
 		Deck.Shuffle (ref deck.cards);
-	}
+
+        layout = GetComponent<Layout>();
+        layout.ReadLayout(layoutXML.text);
+        drawPile = ConvertListCardsToListCardProspectors(deck.cards);
+        LayoutGame();
+    }
+
+    CardProspector Draw()
+    {
+        CardProspector cd = drawPile[0];
+        drawPile.RemoveAt(0);
+        return (cd);
+    }
+
+    CardProspector FindCardByLayoutID(int layoutID)
+    {
+        foreach (CardProspector tCP in tableau)
+        {
+            if(tCP.layoutID == layoutID)
+            {
+                return (tCP);
+            }
+        }
+        return null;
+    }
+
+    void LayoutGame()
+    {
+        if(layoutAnchor == null)
+        {
+            GameObject tGO = new GameObject("_LayoutAnchor");
+            layoutAnchor = tGO.transform;
+            layoutAnchor.transform.position = layoutCenter;
+        }
+
+        CardProspector cp;
+
+        foreach(SlotDef tSD in layout.slotDefs)
+        {
+            cp = Draw();
+            cp.faceUp = tSD.faceUp;
+            cp.transform.parent = layoutAnchor;
+            cp.transform.localPosition = new Vector3(
+                layout.multiplier.x * tSD.x,
+                layout.multiplier.y * tSD.y,
+                -tSD.layerID);
+            cp.layoutID = tSD.id;
+            cp.slotDef = tSD;
+            cp.state = CardState.tableau;
+
+            cp.SetSortingLayerName(tSD.layerName);
+
+            tableau.Add(cp);
+        }
+
+        foreach(CardProspector tCP in tableau)
+        {
+            foreach(int hid in tCP.slotDef.hiddenBy)
+            {
+                cp = FindCardByLayoutID(hid);
+                tCP.hiddenBy.Add(cp);
+            }
+        }
+
+        MoveToTarget(Draw());
+        UpdateDrawPile();
+    }
+
+    List<CardProspector> ConvertListCardsToListCardProspectors(List<Card> lCD)
+    {
+        List<CardProspector> lCP = new List<CardProspector>();
+        CardProspector tCP;
+        foreach(Card tCD in lCD)
+        {
+            tCP = tCD as CardProspector;
+            lCP.Add(tCP);
+        }
+        return (lCP);
+
+    }
+
+    public void CardClicked(CardProspector cd)
+    {
+        switch (cd.state)
+        {
+            case CardState.target:
+                break;
+
+            case CardState.drawpile:
+                MoveToDiscard(target);
+                MoveToTarget(Draw());
+                UpdateDrawPile();
+                break;
+
+            case CardState.tableau:
+                bool validMatch = true;
+                if (!cd.faceUp)
+                {
+                    validMatch = false;
+                }
+                if(!AdjacentRank(cd, target))
+                {
+                    validMatch = false;
+                }
+                if (!validMatch) return;
+                tableau.Remove(cd);
+                MoveToTarget(cd);
+                SetTableauFaces();
+                break;
+        }
+    }
+
+    void MoveToDiscard(CardProspector cd)
+    {
+        cd.state = CardState.discard;
+        discardPile.Add(cd);
+        cd.transform.parent = layoutAnchor;
+        cd.transform.localPosition = new Vector3(
+            layout.multiplier.x * layout.discardPile.x,
+            layout.multiplier.y * layout.discardPile.y,
+            -layout.discardPile.layerID + 0.5f);
+        cd.faceUp = true;
+        cd.SetSortingLayerName(layout.discardPile.layerName);
+        cd.SetSortOrder(-100 + discardPile.Count);
+    }
+
+    void MoveToTarget(CardProspector cd)
+    {
+        if (target != null) MoveToDiscard(target);
+        target = cd;
+        cd.state = CardState.target;
+        cd.transform.parent = layoutAnchor;
+        cd.transform.localPosition = new Vector3(
+            layout.multiplier.x * layout.discardPile.x,
+            layout.multiplier.y * layout.discardPile.y,
+            -layout.discardPile.layerID);
+        cd.faceUp = true;
+        cd.SetSortingLayerName(layout.discardPile.layerName);
+        cd.SetSortOrder(0);
+    }
+
+    void UpdateDrawPile()
+    {
+        CardProspector cd;
+        for(int i=0; i<drawPile.Count; i++)
+        {
+            cd = drawPile[i];
+            cd.transform.parent = layoutAnchor;
+            Vector2 dpStagger = layout.drawPile.stagger;
+            cd.transform.localPosition = new Vector3(
+                layout.multiplier.x * (layout.drawPile.x + i * dpStagger.x),
+                layout.multiplier.y * (layout.drawPile.y + i * dpStagger.y),
+                -layout.drawPile.layerID + 0.1f * i);
+            cd.faceUp = false;
+            cd.state = CardState.drawpile;
+            cd.SetSortingLayerName(layout.drawPile.layerName);
+            cd.SetSortOrder(-10 * i);
+        }
+    }
+
+    public bool AdjacentRank(CardProspector c0, CardProspector c1)
+    {
+        if (!c0.faceUp || !c1.faceUp) return (false);
+        
+        if(Mathf.Abs(c0.rank - c1.rank) == 1)
+        {
+            return (true);
+        }
+
+        if (c0.rank == 1 && c1.rank == 13) return (true);
+
+        if (c0.rank == 13 && c1.rank == 1) return (true);
+
+        return (false);
+    }
+
+    void SetTableauFaces()
+    {
+        foreach(CardProspector cd in tableau)
+        {
+            bool fup = true;
+
+            foreach(CardProspector cover in cd.hiddenBy)
+            {
+                if(cover.state == CardState.tableau)
+                {
+                    fup = false;
+                }
+            }
+            cd.faceUp = fup;
+        }
+    }
 
 }
